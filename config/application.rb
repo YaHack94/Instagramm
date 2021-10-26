578c95620e573a7a185a7e6d77fca2e8adc47536
YaHack94 <yahack94@gmail.com>
2021-10-26 18:58:01 +0100

    ldcjdsjk

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Instagramm
 
     # Configuration for the application, engines, and railties goes here.
     #
+    config.assets.initialize_on_precompile = false
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
