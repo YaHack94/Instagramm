53ea09dac4245c2e5458ffea2b48a1bcf2f058c3
YaHack94 <yahack94@gmail.com>
2021-11-02 11:13:18 +0100

    Commentaire Gemfile supprimé et Pages d'erreurs 404 et 500 généré

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get '/' => 'users#index'
   get '/', to: 'users#index', as: 'user'
   get '/new_user', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   post '/sessions' => 'sessions#create'
-  #post '/users' => 'users#create'
   get '/dashboard' => 'accounts#index'
   post '/dashboard/publication', to: 'accounts#create', as: 'createpub'
   get '/logout', to: 'sessions#destroy', as: 'destroy'
   get 'user/:id/edit', to: 'users#update', as: 'editform'
   post 'user/:id/edit', to: 'users#update', as: 'modifier'
-
+  get  '*path', controller: 'application', action: 'render_404'
+  post '*path', controller: 'application', action: 'render_404'
+  get  '*path', controller: 'application', action: 'render_500'
+  post '*path', controller: 'application', action: 'render_500'
   resources :posts, only: [:new,:create,:show]
 end
